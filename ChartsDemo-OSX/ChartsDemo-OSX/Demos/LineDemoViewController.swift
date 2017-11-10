dd3930249c11d22a56f8bebbc3a041ffccae4166
Pouria B. Almassi <pouriaba@gmail.com>
2017-11-10 13:49:35 +1100

    Restore LineDemoViewController.

--- ChartsDemo-OSX/ChartsDemo-OSX/Demos/LineDemoViewController.swift
+++ ChartsDemo-OSX/ChartsDemo-OSX/Demos/LineDemoViewController.swift
@@ -12,46 +12,6 @@ import Foundation
 import Cocoa
 import Charts
 
-open class FanLineDemoViewController: NSViewController
-{
-    @IBOutlet var lineChartView: LineChartView!
-
-    override open func viewDidLoad()
-    {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let ys1 = Array(1..<100).map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) }
-        let ys2 = Array(1..<100).map { x in return cos(Double(x) / 2.0 / 3.141) }
-
-        let yse1 = ys1.enumerated().map { x, y in return ChartDataEntry(x: Double(x), y: y) }
-        let yse2 = ys2.enumerated().map { x, y in return ChartDataEntry(x: Double(x), y: y) }
-
-        let data = LineChartData()
-        let ds1 = LineChartDataSet(values: yse1, label: "Hello")
-        ds1.colors = [NSUIColor.red]
-        data.addDataSet(ds1)
-
-        let ds2 = LineChartDataSet(values: yse2, label: "World")
-        ds2.colors = [NSUIColor.blue]
-
-        ds2.fillFormatter = FanFillFormatter(boundaryDataSet: ds1)
-
-        data.addDataSet(ds2)
-        self.lineChartView.data = data
-
-        self.lineChartView.gridBackgroundColor = NSUIColor.white
-
-        self.lineChartView.chartDescription?.text = "Linechart Demo"
-    }
-
-    override open func viewWillAppear()
-    {
-        self.lineChartView.animate(xAxisDuration: 0.0, yAxisDuration: 1.0)
-    }
-}
-
-
 open class LineDemoViewController: NSViewController
 {
     @IBOutlet var lineChartView: LineChartView!
